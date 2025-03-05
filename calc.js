2cc9da3ed7acde2cf0c0b3e26bacbe54a21985fc
MadupuHiranya07 <madupuhiranya07@gmail.com>
2025-03-05 13:36:21 +0530

    new

---
+++ calc.js
@@ -0,0 +1,9 @@
+function greet(){
+    return "Hello hiranya"
+}
+
+add(a,b){
+    return a+b
+}
+
+export{add}
---
+++ main.js
@@ -0,0 +1,6 @@
+import greet from "./calc.js"
+import {add} from "./calc.js"
+let result=greet()
+console.log(result)
+let res1=add(4,6)
+console.log(res1)
